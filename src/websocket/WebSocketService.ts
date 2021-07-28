164b77becb6b8a7a5022691b14bb7427177c1933
Rubens Junqueira <rubens.junqueira@outlook.com>
2021-07-28 19:58:45 -0300

    create start_chat event

--- src/websocket/WebSocketService.ts
+++ src/websocket/WebSocketService.ts
@@ -1,8 +1,11 @@
 import { container } from 'tsyringe';
 
 import { io } from '../http';
+import { CreateChatRoomService } from '../services/CreateChatRoomService';
 import { CreateUserService } from '../services/CreateUserService';
 import { GetAllUsersService } from '../services/GetAllUsersService';
+import { GetChatRoomByUsersService } from '../services/GetChatRoomByUsersService';
+import { GetUserBySocketIdService } from '../services/GetUserBySocketIdService';
 
 io.on('connect', (socket) => {
 	socket.emit('chat_iniciado', {
@@ -29,5 +32,28 @@ io.on('connect', (socket) => {
 		const users = await getAllUsersService.execute();
 
 		callback(users);
+	});
+
+	socket.on('start_chat', async (data, callback) => {
+		const createChatRoomService = container.resolve(CreateChatRoomService);
+		const getUserBySocketIdService = container.resolve(
+			GetUserBySocketIdService
+		);
+		const getChatRoomByUsersService = container.resolve(
+			GetChatRoomByUsersService
+		);
+
+		const userLogged = await getUserBySocketIdService.execute(socket.id);
+
+		let room = await getChatRoomByUsersService.execute([
+			userLogged.id,
+			data.idUser,
+		]);
+
+		if (!room) {
+			room = await createChatRoomService.execute([data.idUser, userLogged.id]);
+		}
+
+		callback(room);
 	});
 });
