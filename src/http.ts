8468f1cf1b10d17bd4bb785f3f8459c38e98181d
Rubens Junqueira <rubens.junqueira@outlook.com>
2021-07-27 22:10:07 -0300

    setup mongodb

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { createServer } from 'http';
+import mongoose from 'mongoose';
 import path from 'path';
 import { Server } from 'socket.io';
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.createConnection('mongodb://localhost/rocketsocket', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
 import { server } from './http';
+import './websocket/WebSocketService';
 
 server.listen(3000, () => console.log('Server is Running'));
